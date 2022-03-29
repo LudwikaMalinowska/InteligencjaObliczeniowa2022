76897700668fb57b5557e17ded1517e5d1bfdee1
Ludwika Malinowska <malinowska.L@wp.pl>
2022-03-29 22:49:43 +0200

    Lab 3 - zadanie 3 - zrobione

--- Lab3/zad3.py
+++ Lab3/zad3.py
@@ -1,13 +1,11 @@
 import math
-
 import pygad
 import numpy
 import time
-import math
 
 
-# S = [1, 2, 3, 6, 10, 17, 25, 29, 30, 41, 51, 60, 70, 79, 80]
-# A = [0.09, 0.06, 0.99, 0.98, 0.1, 0.15]
+
+
 
 
 labirynt = [
@@ -25,38 +23,99 @@ labirynt = [
     [1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 
+#0 - prawo, 1 - lewo, 2 - góra, 3 - dół
+steps_dict = {
+    0: "Prawo",
+    1: "Lewo",
+    2: "Góra",
+    3: "Dół"
+}
+
 #definiujemy parametry chromosomu
 #geny to liczby: 0 lub 1
 gene_space = [0, 1, 2, 3]
 # gene_space = {"low": 0, "high": 1, "step": 0.01}
 
 
-def endurance(x, y, z, u, v, w):
-    return math.exp(-2*(y-math.sin(x))**2)+math.sin(z*u)+math.cos(v*w)
 
+
+def filter_steps(steps):
+    curr = {"x": 1, "y": 1}
+    filtered_steps = []
+    for i in range(len(steps)):
+        step = steps[i]
+        if step == "Prawo":
+            nField = {"x": curr["x"], "y": curr["y"] + 1}
+        elif step == "Lewo":
+            nField = {"x": curr["x"], "y": curr["y"] - 1}
+        elif step == "Góra":
+            nField = {"x": curr["x"] - 1, "y": curr["y"]}
+        else:
+            nField = {"x": curr["x"] + 1, "y": curr["y"]}
+
+        nextField = labirynt[nField["x"]][nField["y"]]
+        if nextField == 0:
+            curr = nField
+            filtered_steps.append(step)
+        elif nextField == 3:
+            filtered_steps.append(step)
+            break
+
+    return filtered_steps
+
+def count_stop(steps):
+    extra_points = 0
+    curr = {"x": 1, "y":1}
+    for i in range(len(steps)):
+        step = steps[i]
+        if step == 0 :
+            nField = {"x": curr["x"], "y": curr["y"] + 1}
+        elif step == 1 :
+            nField = {"x": curr["x"], "y": curr["y"] - 1}
+        elif step == 2 :
+            nField = {"x": curr["x"] - 1, "y": curr["y"]}
+        else:
+            nField = {"x": curr["x"] + 1, "y": curr["y"]}
+
+        nextField = labirynt[nField["x"]][nField["y"]]
+
+        if nField == curr:
+            extra_points -= 0.2
+
+        if nextField == 0:
+            curr = nField
+        elif nextField == 3:
+            curr = nField
+            extra_points += (len(steps) - i)
+            break
+
+
+    return curr, extra_points
 
 #definiujemy funkcjÄ fitness
 def fitness_func(solution, solution_idx):
-    sum1 = endurance(*solution)
-    # solution_invert = 1 - solution
-    # sum2 = endurance(solution_invert)
-    # fitness = -numpy.abs(sum1-sum2)
-    #lub: fitness = 1.0 / (1.0 + numpy.abs(sum1-sum2))
-    return sum1
+    stop, extra_points= count_stop(solution)
+    end = {"x": 10, "y": 10}
+    odl = (-1) * (abs(stop["x"] - end["x"]) + abs(stop["y"] - end["y"]))
+
+    if odl == 0:
+        return odl + extra_points + 10
+    else:
+        return odl + extra_points
 
 fitness_function = fitness_func
 
 #ile chromsomĂłw w populacji
 #ile genow ma chromosom
 sol_per_pop = 20
-num_genes = len(A)
+num_genes = 30
 
 #ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
 #ile pokolen
 #ilu rodzicow zachowac (kilka procent)
-num_parents_mating = 5
-num_generations = 30
-keep_parents = 2
+num_parents_mating = 10
+num_generations = 100
+keep_parents = 4
 
 #jaki typ selekcji rodzicow?
 #sss = steady, rws=roulette, rank = rankingowa, tournament = turniejowa
@@ -68,7 +127,7 @@ crossover_type = "single_point"
 #mutacja ma dzialac na ilu procent genow?
 #trzeba pamietac ile genow ma chromosom
 mutation_type = "random"
-mutation_percent_genes = 17
+mutation_percent_genes = 5
 
 #inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
 ga_instance = pygad.GA(gene_space=gene_space,
@@ -82,7 +141,7 @@ ga_instance = pygad.GA(gene_space=gene_space,
                        crossover_type=crossover_type,
                        mutation_type=mutation_type,
                        mutation_percent_genes=mutation_percent_genes,
-                       stop_criteria=["reach_0"]
+                       # stop_criteria=["reach_15"]
                        )
 
 start = time.time()
@@ -95,12 +154,15 @@ print(end - start)
 
 #podsumowanie: najlepsze znalezione rozwiazanie (chromosom+ocena)
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
+solution = list(map(lambda x: steps_dict[x], solution))
+fsolution = filter_steps(solution)
 print("Parameters of the best solution : {solution}".format(solution=solution))
+print(f"Filtered solution : {fsolution}")
 print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
 #tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
-prediction = numpy.sum(solution)
-print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+# prediction = numpy.sum(solution)
+# print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
 
 #wyswietlenie wykresu: jak zmieniala sie ocena na przestrzeni pokolen
 ga_instance.plot_fitness()
