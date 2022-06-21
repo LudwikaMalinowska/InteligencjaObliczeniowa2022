a80cbf14b587ceed7044f5b1b091b965c78e0339
Ludwika Malinowska <malinowska.L@wp.pl>
2022-06-21 17:30:33 +0200

    preparing files for scrapping weeks0-6, week 0-3 scrapped

---
+++ Projekt2/week0/h2_amber_pos.py
@@ -0,0 +1,106 @@
+
+def my_filter(elem: str):
+    if "https://t.co/" in elem:
+        return False
+    return True
+
+my_file = open("scrapper/hashtags_amberheard_pos.txt", "r")
+content_list = my_file.readlines()
+content_list = filter(my_filter, content_list)
+text = " ".join(content_list)
+
+from nltk.tokenize import sent_tokenize
+tokenized_text=sent_tokenize(text)
+# print(tokenized_text)
+
+from nltk.tokenize import word_tokenize
+tokenized_word=word_tokenize(text)
+# print(tokenized_word)
+
+from nltk.probability import FreqDist
+fdist = FreqDist(tokenized_word)
+print(fdist.items())
+
+print(fdist.most_common(5))
+
+
+
+
+from nltk.corpus import stopwords
+stop_words=set(stopwords.words("english"))
+stop_words.add(".")
+stop_words.add(",")
+stop_words.add('"')
+stop_words.add("'")
+stop_words.add("'s")
+stop_words.add("``")
+stop_words.add("''")
+stop_words.add("#")
+stop_words.add("@")
+stop_words.add("!")
+stop_words.add("’")
+stop_words.add("?")
+stop_words.add(";")
+stop_words.add("amp")
+stop_words.add("&")
+stop_words.add("n't")
+# print(stop_words)
+
+filtered_sent=[]
+for w in tokenized_word:
+    if w.lower() not in stop_words:
+        filtered_sent.append(w)
+print("Tokenized Sentence:",tokenized_word)
+print("Filterd Sentence:",filtered_sent)
+
+
+#Lexicon Normalization
+#performing stemming and Lemmatization
+
+from nltk.stem.wordnet import WordNetLemmatizer
+lem = WordNetLemmatizer()
+
+
+
+print()
+print()
+fdist = FreqDist(filtered_sent)
+print("Wektor słów:")
+print(fdist.items())
+
+print("10 most common words:")
+print(fdist.most_common(10))
+common10vector = fdist.most_common(10)
+labelsX = [x for (x, y) in common10vector]
+labelsY = [y for (x, y) in common10vector]
+
+
+
+# Frequency Distribution Plot
+import matplotlib.pyplot as plt
+fdist.plot(10,cumulative=False)
+plt.show()
+
+plt.bar(labelsX, labelsY)
+plt.show()
+
+
+
+
+# Start with loading all necessary libraries
+import numpy as np
+import pandas as pd
+from os import path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+import matplotlib.pyplot as plt
+
+
+# Create and generate a word cloud image:
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
---
+++ Projekt2/week3/week0/scrapper/hashtag3.py
@@ -0,0 +1,63 @@
+# importing libraries and packages
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# Creating list to append tweet data
+tweets_list1 = []
+
+
+for i, tweet in enumerate(sntwitter.TwitterHashtagScraper("JusticeForJohnnyDepp since:2022-04-25 until:2022-05-01 lang:en")
+                                  .get_items()):
+    if i > 10000:  # number of tweets you want to scrape
+        break
+    # print(tweet)
+    tweets_list1.append(
+        [tweet.date, tweet.id, tweet.content, tweet.username])  # declare the attributes to be returned
+
+tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+
+print(tweets_df1.head())
+
+
+
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid = SentimentIntensityAnalyzer()
+for tweet in tweets_df1["Text"].head().tolist():
+    ss = sid.polarity_scores(tweet)
+    for k in sorted(ss):
+        print('{0}: {1}, '.format(k, ss[k]), end='')
+        print()
+
+# ss = sid.polarity_scores(tweets_df1)
+# for k in sorted(ss):
+#     print('{0}: {1}, '.format(k, ss[k]), end='')
+#     print()
+
+
+with open('hashtags_JusticeForJohnnyDepp.txt', 'w') as the_file:
+    for tweet in tweets_df1["Text"].tolist():
+        # print(tweet)
+        the_file.write(tweet)
+        the_file.write("\n")
+
+pos_number = 0
+neg_number = 0
+with open('hashtags_JusticeForJohnnyDepp_pos.txt', 'w') as the_file_pos:
+    with open('hashtags_JusticeForJohnnyDepp_neg.txt', 'w') as the_file_neg:
+        for tweet in tweets_df1["Text"].tolist():
+            ss = sid.polarity_scores(tweet)
+
+            if ss["pos"] > ss["neg"]:
+                the_file_pos.write(tweet)
+                the_file_pos.write("\n")
+                pos_number += 1
+            elif ss["pos"] < ss["neg"]:
+                the_file_neg.write(tweet)
+                the_file_neg.write("\n")
+                neg_number += 1
+
+print(pos_number)
+print(neg_number)
+print(len(tweets_list1))
+
